75a7374a5336e6b70cbe781fb19ff8a57fe4ee52
Roman Osipov <roman@MacBook-Air-Roman.local>
2025-08-15 15:51:56 +0300

    Fix missing tunnel routes in index.php - Add TunnelController import and register all tunnel routes

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use App\Controllers\DashboardController;
 use App\Controllers\SystemController;
 use App\Controllers\ProcessController;
 use App\Controllers\ServiceController;
+use App\Controllers\TunnelController;
 
 // Загружаем переменные окружения
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
@@ -19,6 +20,12 @@ $app->router->get('/', [DashboardController::class, 'index']);
 $app->router->get('/system', [SystemController::class, 'index']);
 $app->router->get('/processes', [ProcessController::class, 'index']);
 $app->router->get('/services', [ServiceController::class, 'index']);
+
+// Маршруты для туннелей
+$app->router->get('/tunnels/ssh', [TunnelController::class, 'ssh']);
+$app->router->get('/tunnels/port-forwarding', [TunnelController::class, 'portForwarding']);
+$app->router->get('/tunnels/wireguard', [TunnelController::class, 'wireguard']);
+$app->router->get('/tunnels/cloudflare', [TunnelController::class, 'cloudflare']);
 
 // Запускаем приложение
 $app->run();
